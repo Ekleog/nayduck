a211af70f734d25da4972fe55e1c06aad5d997ba
Léo Gaspard <leo@gaspard.io>
2022-05-05 17:04:59 +0200

    wip

--- fuzzers/main.py
+++ fuzzers/main.py
@@ -39,13 +39,13 @@ SYNC_LOG_UPLOAD_INTERVAL = datetime.timedelta(3600)
 
 def connect_to_gcs() -> gcs.Client:
     """Setup the environment to have gsutils work, and return a connection to GCS"""
-    subprocess.run(
-        [
-            'gcloud', 'auth', 'activate-service-account', '--key-file',
-            GCS_CREDENTIALS_FILE
-        ],
-        check=True,
-    )
+    #subprocess.run(
+    #    [
+    #        'gcloud', 'auth', 'activate-service-account', '--key-file',
+    #        GCS_CREDENTIALS_FILE
+    #    ],
+    #    check=True,
+    #)
     return gcs.Client.from_service_account_json(str(GCS_CREDENTIALS_FILE))
     #return gcs.Client(project = 'near-nayduck')
 
